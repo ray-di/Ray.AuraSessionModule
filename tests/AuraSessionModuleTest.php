4b3ff72899495dc08e10d8e0ad7b2f73410fa4c1
Kouhei.Sano <apple.x.co@gmail.com>
2024-06-27 17:41:23 +0900

    chore: Add red test

--- tests/AuraSessionModuleTest.php
+++ tests/AuraSessionModuleTest.php
@@ -12,12 +12,22 @@ use Aura\Session\Session;
 use PHPUnit\Framework\TestCase;
 use Ray\Di\Injector;
 
+use function serialize;
+
 class AuraSessionModuleTest extends TestCase
 {
-    public function testAuraSessionModule()
+    public function testAuraSessionModule(): void
     {
         $injector = new Injector(new AuraSessionModule());
         $session = $injector->getInstance(Session::class);
         $this->assertInstanceOf(Session::class, $session);
+    }
+
+    public function testSerialize(): void
+    {
+        $injector = new Injector(new AuraSessionModule());
+        $session = $injector->getInstance(Session::class);
+        $serialized = serialize($session);
+        $this->assertIsString($serialized);
     }
 }
